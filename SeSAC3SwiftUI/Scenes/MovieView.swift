eba7ff3952c64087453782f771008c42550b3103
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-11-14 14:33:46 +0900

    feat: ZStack, VStack, HStack 실습

--- SeSAC3SwiftUI/Scenes/MovieView.swift
+++ SeSAC3SwiftUI/Scenes/MovieView.swift
@@ -9,7 +9,38 @@ import SwiftUI
 
 struct MovieView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("dummy-forest-fall")
+                .resizable()
+                .ignoresSafeArea()
+            VStack(spacing: 20) {
+                Image("dummy-bird")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .border(.blue, width: 5)
+                Text("zzzz")
+                    .font(.largeTitle)
+                    .foregroundColor(.black)
+                    .bold()
+            }
+            VStack {
+                Spacer()
+                HStack {
+                    Image("dummy-bird")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .border(.blue, width: 5)
+                    Image("dummy-bird")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .border(.blue, width: 5)
+                    Image("dummy-bird")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .border(.blue, width: 5)
+                }
+            }
+        }
     }
 }
 
@@ -18,3 +49,48 @@ struct MovieView_Previews: PreviewProvider {
         MovieView()
     }
 }
+
+
+/* 상중하 연습
+ struct MovieView: View {
+     var body: some View {
+         ZStack {
+             Image("dummy-forest-fall")
+                 .resizable()
+                 .ignoresSafeArea()
+             VStack(spacing: 20) {
+                 Image("dummy-bird")
+                     .resizable()
+                     .frame(width: 100, height: 100)
+                     .border(.blue, width: 5)
+                 Text("zzzz")
+                     .font(.largeTitle)
+                     .foregroundColor(.black)
+                     .bold()
+                 Spacer()
+                 HStack {
+                     Circle()
+                     Rectangle()
+                         .fill(.blue)
+                     RoundedRectangle(cornerRadius: 50)
+                         .fill(.yellow)
+                 }
+                 HStack {
+                     Image("dummy-bird")
+                         .resizable()
+                         .frame(width: 100, height: 100)
+                         .border(.blue, width: 5)
+                     Image("dummy-bird")
+                         .resizable()
+                         .frame(width: 100, height: 100)
+                         .border(.blue, width: 5)
+                     Image("dummy-bird")
+                         .resizable()
+                         .frame(width: 100, height: 100)
+                         .border(.blue, width: 5)
+                 }
+             }
+         }
+     }
+ }
+ */
