f4c8bf0609bf1ffdcefb34acb09b181edcbfcb61
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-11-14 15:35:08 +0900

    feat: NavigationView 및 Gradient 실습

--- SeSAC3SwiftUI/Scenes/RenderView.swift
+++ SeSAC3SwiftUI/Scenes/RenderView.swift
@@ -12,19 +12,41 @@ struct RenderView: View {
     @State var age = 10
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 20) {
-            Text("Andy: \(Int.random(in: 1...99))")
-            NoahView(name: "Noah")
-            ssaboo
-            Text("Joey: \(Int.random(in: 1...99))")
-            Button("CLICK") {
-                print("CLICKED")
-                age = Int.random(in: 1...100)
+        NavigationView {
+            VStack(alignment: .leading, spacing: 20) {
+                HStack {
+                    RoundedRectangle(cornerRadius: 30)
+                        .fill(
+                            LinearGradient(gradient: Gradient(colors: [.red, .blue, .yellow]), startPoint: .leading, endPoint: .trailing)
+                        )
+                    RoundedRectangle(cornerRadius: 30)
+                        .fill(
+                            RadialGradient(gradient: Gradient(colors: [.green, .red, .yellow]), center: .topLeading, startRadius: 20, endRadius: 300)
+                        )
+                    RoundedRectangle(cornerRadius: 30)
+                        .fill(
+                            AngularGradient(colors: [.yellow, .blue], center: .leading, angle: .degrees(0))
+                        )
+                }
+                .frame(width: .infinity, height: 100)
+                NavigationLink("Push") {
+                    MenuView()
+                }
+                Text("Andy: \(Int.random(in: 1...99))")
+                NoahView(name: "Noah")
+                ssabooView
+                Text("Joey: \(Int.random(in: 1...99))")
+                Button("CLICK") {
+                    print("CLICKED")
+                    age = Int.random(in: 1...100)
+                }
             }
+            .navigationTitle("TITLE")
+            // 가장 바깥 영역이 아닌 NavigationView 끝에 위치해야 함
         }
     }
     
-    var ssaboo: some View {
+    var ssabooView: some View {
         Text("Ssaboo: \(Int.random(in: 1...99))")
     }
 }
