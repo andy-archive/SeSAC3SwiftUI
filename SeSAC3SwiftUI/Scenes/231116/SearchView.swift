811187c0706b6e42bef0c46418059006564c432c
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-11-16 19:30:00 +0900

    feat: 하위 뷰의 값 변경에 따른 화면 업데이트 연습

--- SeSAC3SwiftUI/Scenes/231116/SearchView.swift
+++ SeSAC3SwiftUI/Scenes/231116/SearchView.swift
@@ -7,58 +7,100 @@
 
 import SwiftUI
 
-/* Movie 모델 */
-struct Movie: Hashable, Identifiable {
-    let id = UUID()
-    let name: String
-    let color = Color.random()
+///* Movie 모델 */
+//struct Movie: Hashable, Identifiable {
+//    let id = UUID()
+//    let name: String
+//    let color = Color.random()
+//}
+//
+///* 새로운 SearchView 231116 1400 */
+//struct SearchView: View {
+//
+//    @State private var query = ""
+//
+//    var filteredMovieList: [Movie] {
+//        return query.isEmpty ? movieList : movieList.filter { movie in
+//            movie.name.contains(query)
+//        }
+//    }
+//
+//    let movieList = [
+//        Movie(name: "A"), Movie(name: "Aab"),
+//        Movie(name: "Avengers"), Movie(name: "Avengers"),
+//        Movie(name: "Harry Potter"), Movie(name: "Eternal Sunshine"),
+//        Movie(name: "Shrek"), Movie(name: "Eternal Sunshine"),
+//        Movie(name: "Mission Impossible"),
+//    ]
+//
+//    var body: some View {
+//
+//        NavigationStack {
+//            List { // LazyVStack(alignment: .leading) {
+//                ForEach(filteredMovieList, id: \.self) { item in // \.id or \.self를 통한 고유성 판단
+//                    NavigationLink(value: item) {
+//                        HStack {
+//                            Circle()
+//                                .foregroundStyle(item.color)
+//                            Text("\(item.name)")
+//                        }
+//                        .frame(height: 60)
+//                    }
+//                }
+//            }
+//            .navigationTitle("Search")
+//            .navigationDestination(for: Movie.self) { item in
+//                SearchDetailView(movie: item)
+//            }
+//        }
+//        .searchable(text: $query, placement: .navigationBarDrawer, prompt: "Insert you want to search")
+//        .onSubmit(of: .search) {
+//            print("SEARCHED")
+//        }
+//    }
+//}
+
+struct SearchView: View {
+
+    @State var randomNum = 0
+
+    init(randomNum: Int = Int.random(in: 1...100)) {
+        print("\(String(describing: self)) init")
+//        self.randomNum = randomNum
+//        print("\(self) init")
+//        print("\(randomNum)")
+    }
+
+    var body: some View {
+        VStack(spacing: 20) {
+            HueView() // new struct
+            jackView // struct property
+            kokoView()// struct method
+            Text("Bran") // Text struct
+                .background(Color.random())
+
+            Text("Andy \(randomNum)")
+                .background(Color.random())
+
+            Button("클릭") { // 클릭 시 매번 뷰가 새로 그려짐
+                //randomNum = Int.random(in: 1...100)
+                print("SearchView Button CLICKED")
+//                    .background(Color.random()
+            }
+        }
+    }
+
+    var jackView: some View {
+        Text("Jack")
+            .background(Color.random())
+    }
+
+    func kokoView() -> some View {
+        Text("Koko")
+            .background(Color.random())
+    }
 }
 
-/* 새로운 SearchView 231116 1400 */
-struct SearchView: View {
-    
-    @State private var query = ""
-    
-    var filteredMovieList: [Movie] {
-        return query.isEmpty ? movieList : movieList.filter { movie in
-            movie.name.contains(query)
-        }
-    }
-    
-    let movieList = [
-        Movie(name: "A"), Movie(name: "Aab"),
-        Movie(name: "Avengers"), Movie(name: "Avengers"),
-        Movie(name: "Harry Potter"), Movie(name: "Eternal Sunshine"),
-        Movie(name: "Shrek"), Movie(name: "Eternal Sunshine"),
-        Movie(name: "Mission Impossible"),
-    ]
-    
-    var body: some View {
-        
-        NavigationStack {
-            List { // LazyVStack(alignment: .leading) {
-                ForEach(filteredMovieList, id: \.self) { item in // \.id or \.self를 통한 고유성 판단
-                    NavigationLink(value: item) {
-                        HStack {
-                            Circle()
-                                .foregroundStyle(item.color)
-                            Text("\(item.name)")
-                        }
-                        .frame(height: 60)
-                    }
-                }
-            }
-            .navigationTitle("Search")
-            .navigationDestination(for: Movie.self) { item in
-                SearchDetailView(movie: item)
-            }
-        }
-        .searchable(text: $query, placement: .navigationBarDrawer, prompt: "Insert you want to search")
-        .onSubmit(of: .search) {
-            print("SEARCHED")
-        }
-    }
-}
 
 struct SearchView_Previews: PreviewProvider {
     static var previews: some View {
@@ -66,33 +108,59 @@ struct SearchView_Previews: PreviewProvider {
     }
 }
 
-struct SearchDetailView: View {
+struct HueView: View {
     
-    let movie: Movie
+    // 이니셜라이저는 계속 실행됨
+    init() {
+        print("\(String(describing: self)) init")
+    }
     
+    @State var randomNum = 0
+    @State var name = "Hue"
+    
+    // 변경되는 부분이 없어서 한번만 실행됨
     var body: some View {
         VStack {
-            Text(movie.name)
-                .font(.title)
-                .padding(20)
-            Button("TITLE & ACTION") {
-                print("\(String(describing: self))")
+            Text(name)
+                .background(Color.random())
+            Button("클릭") { // 클릭 시 매번 뷰가 새로 그려짐
+                name = "\(Int.random(in: 1...1000))" // 값을 바꿨으니 업데이트 됨 // "Hue"
+//                print("============")
+//                print(randomNum)
+//                randomNum = Int.random(in: 1...1000)
+//                print(randomNum)
             }
-            Button {
-                print("ACTION & LABEL")
-            } label: {
-                HStack {
-                    Circle()
-                        .foregroundStyle(Color.random())
-                    Text("ACTION & LABEL")
-                        .navigationTitle("Search")
-                }
-                .frame(width: 140)
-            }
-
         }
     }
 }
+
+//struct SearchDetailView: View {
+//
+//    let movie: Movie
+//
+//    var body: some View {
+//        VStack {
+//            Text(movie.name)
+//                .font(.title)
+//                .padding(20)
+//            Button("TITLE & ACTION") {
+//                print("\(String(describing: self))")
+//            }
+//            Button {
+//                print("ACTION & LABEL")
+//            } label: {
+//                HStack {
+//                    Circle()
+//                        .foregroundStyle(Color.random())
+//                    Text("ACTION & LABEL")
+//                        .navigationTitle("Search")
+//                }
+//                .frame(width: 140)
+//            }
+//
+//        }
+//    }
+//}
 
 /* NavigationView → NavigationLink(destination) & Detail
  NavigationView {
@@ -196,20 +264,6 @@ struct SearchDetailView: View {
 //    }
 //}
 
-
-struct HueView: View {
-    
-    // 이니셜라이저는 계속 실행됨
-    init() {
-        print("\(self) init")
-    }
-    
-    // 변경되는 부분이 없어서 한번만 실행됨
-    var body: some View {
-        Text("Hue")
-            .background(Color.random())
-    }
-}
 
 ///MARK: Color
 
