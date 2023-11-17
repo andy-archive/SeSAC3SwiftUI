8501762123fad510441c0e8c197a76425850d714
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-11-17 11:04:59 +0900

    feat: @ViewBuilder를 이용한 View의 중첩 구현

--- SeSAC3SwiftUI/Scenes/231117/ChartView.swift
+++ SeSAC3SwiftUI/Scenes/231117/ChartView.swift
@@ -10,11 +10,34 @@ import Charts
 
 struct ChartView: View {
     
+    @Environment(\.colorScheme) var colorScheme
+    
     let movieList: [Movie]
+    
+    @ViewBuilder
+    var chartTitle: some View {
+        /* 기존 방식 */
+        //Text(colorScheme == .dark ? "DARK CHART" : "LIGHT CHART")
+        
+        /* @ViewBuilder를 통한 해결
+         ERROR: Function declares an opaque return type, but has no return statements in its body from which to infer an underlying type
+         */
+        if colorScheme == .dark {
+            HStack {
+                Image(systemName: "moon.fill")
+                Text("DARK CHART")
+            }
+        } else {
+            HStack {
+                Image(systemName: "sun.max.fill")
+                Text("LIGHT CHART")
+            }
+        }
+    }
     
     var body: some View {
         VStack {
-            Text("CHART")
+            chartTitle
             Chart(movieList, id: \.self) { item in
                 BarMark( //LineMark //RectangleMark // AreaMark
                     x: .value("Genre", item.name),
